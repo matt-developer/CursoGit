9d703e8aab3622c2a34d587b68f29e3874225345
Mateus da Silva Pereira <mateusdev.cs@gmail.com>
2021-10-23 21:28:21 -0300

    Projeto pronto, com todas as funcionalidades em perfeito estado!

---
+++ GitTeste/Empregados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace GitTeste
+{
+    class Empregados
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Empregados(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void EncrementoSalario(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Nome}, {Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ GitTeste/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace GitTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos empregados serão registrados ? ");
+            int q = int.Parse(Console.ReadLine());
+
+            List<Empregados> empregados = new List<Empregados>();
+
+            for (int lista = 1; lista <= q; lista++)
+            {
+                Console.WriteLine($"Empregado #{lista}:");
+
+                Console.Write("Id : ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Nome : ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Salário : ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                empregados.Add(new Empregados(id, nome, salario));
+                Console.WriteLine();
+            }
+
+            Console.Write("Informe a ID que receberá acrescimo : ");
+            int procurarId = int.Parse(Console.ReadLine());
+
+            Empregados funcionarios = empregados.Find(x => x.Id == procurarId);
+
+            if (funcionarios != null)
+            {
+                Console.Write("Informe a porcentagem : ");
+                double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                funcionarios.EncrementoSalario(porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("Essa ID não existe ! ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista Atualizadas dos Empregados ");
+
+            foreach (Empregados emp in empregados)
+            {
+                Console.WriteLine(emp);
+            }
+        }
+    }
+}
